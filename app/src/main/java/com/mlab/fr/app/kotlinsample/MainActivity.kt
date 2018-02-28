9c592b7cff914c9956ff46557881d238697c4e43
Gopi Karmakar <gopi.karmakar@gmail.com>
2018-02-28 17:39:49 +0900

    Initial Commit

---
+++ app/src/main/java/com/mlab/fr/app/kotlinsample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mlab.fr.app.kotlinsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
